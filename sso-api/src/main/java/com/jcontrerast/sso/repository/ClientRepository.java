63a82bc5113d8f9b79079872490087c89bec32d9
luis-tapia12 <j.contreras.t@hotmail.com>
2024-05-19 04:36:22 -0600

    add authorization server config

--- sso-api/src/main/java/com/jcontrerast/sso/repository/ClientRepository.java
+++ sso-api/src/main/java/com/jcontrerast/sso/repository/ClientRepository.java
@@ -3,5 +3,8 @@ package com.jcontrerast.sso.repository;
 import com.jcontrerast.sso.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByClientId(String clientId);
 }
--- sso-api/src/main/java/com/jcontrerast/sso/service/impl/ClientServiceImpl.java
+++ sso-api/src/main/java/com/jcontrerast/sso/service/impl/ClientServiceImpl.java
@@ -7,10 +7,17 @@ import com.jcontrerast.sso.service.ClientService;
 import com.jcontrerast.sso.utils.Utils;
 import org.springframework.data.domain.Page;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
-public class ClientServiceImpl implements ClientService {
+public class ClientServiceImpl implements ClientService, RegisteredClientRepository {
     private final ClientRepository repository;
     private final PasswordEncoder passwordEncoder;
 
@@ -54,5 +61,34 @@ public class ClientServiceImpl implements ClientService {
         } else {
             throw new RuntimeException("The client with id " + id + " does not exist");
         }
+    }
+
+    @Override
+    public void save(RegisteredClient registeredClient) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return buildRegisteredClient(getClient(id));
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+        return buildRegisteredClient(repository.findByClientId(clientId)
+                .orElseThrow(() -> new RuntimeException("The client with client id " + clientId + " does not exist")));
+    }
+
+    private RegisteredClient buildRegisteredClient(Client client) {
+        return RegisteredClient.withId(client.getId())
+                .clientName(client.getName())
+                .clientId(client.getClientId())
+                .clientSecret(client.getPassword())
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri(client.getRedirectUrl())
+                .scope(OidcScopes.OPENID)
+                .build();
     }
 }
