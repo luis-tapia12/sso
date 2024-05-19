ef8f7960789cfdf8db90f8626751202cd3c9622d
luis-tapia12 <j.contreras.t@hotmail.com>
2024-05-19 05:49:55 -0600

    add react app

--- sso-api/src/main/java/com/jcontrerast/sso/utils/Constants.java
+++ sso-api/src/main/java/com/jcontrerast/sso/utils/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
     public final static int DEFAULT_PAGE_NUMBER = 0;
 
     public final static String USER_NAME_REGEX = "^(?!.*[._-]{2})(?![0-9])[a-zA-Z0-9]+(?:[._-][a-zA-Z0-9]+)*$";
+
+    public final static String APP_BASE_HOLDER = "{0}/app";
+    public final static String APP_INDEX_HOLDER = APP_BASE_HOLDER + "/index.html";
 }
