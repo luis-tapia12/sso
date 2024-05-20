d6c4d581eaa2c6680cfea5f5ef8e719300747d7a
luis-tapia12 <j.contreras.t@hotmail.com>
2024-05-19 22:29:35 -0600

    add prettier config

--- sso-gui/vite.config.ts
+++ sso-gui/vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    base: env.BASE_URL,
-    plugins: [react()],
-  };
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		base: env.BASE_URL,
+		plugins: [react()]
+	};
 });
